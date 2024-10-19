3f04d5ae45461bf32eab89c7caf1ab7ca5912527
xXElSeRriiOoXx <marco@gmail.com>
2024-10-19 10:38:22 -0600

    arrayList

--- resources/views/crearNota.blade.php
+++ resources/views/crearNota.blade.php
@@ -5,16 +5,16 @@
 @endsection
 
 @section("contenido")
+<form action="">
 <div class="tile">
   <h3 class="tile-title"></h3>
   <div class="d-flex justify-content-end">
     <div class="form-check">
-      <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+      <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
       <label class="form-check-label" for="flexCheckChecked">
        Require Factura
       </label>
     </div>
-
   </div>
   <table class="table table-bordered">
     <thead>
@@ -29,7 +29,7 @@
     <tbody>
       <tr>
         <td>
-            <input class="form-control" type="text" placeholder="Nota "> 
+            <input class="form-control" type="text" placeholder="Nota 12" disabled>
         </td>
         <td>
           <input class="form-control" type="text" placeholder="Contacto ">
@@ -48,95 +48,120 @@
         </td>
       </tr>
       <tr class="table-success">
-         <td id="txt_cantidad"><a class="btn btn-primary" href="#"><i class="bi bi-plus-square fs-5"></i></a> Cantidad</td>
-        <td id="txt_descripcion">Descripcion</td>
-        <td id="txtPreciounitario">Precio Unitario</td>
-        <td id="txtTotal">Total</td>
-        <td>Entrega en</td>
+            <th colspan="3">Public G</th>
+            <th colspan="2">Concepto</th>
       </tr>
       <tr>
-        <td> <input class="form-control" type="text" placeholder="Cantidad "></td>
-        <td>
-          <input class="form-control" type="text" placeholder="Descipcion ">
-        </td>
-        <td>
-          <input class="form-control" type="text" placeholder="Total ">
-        </td>
-        <td>
-          <label class="form-label">$0.00</label>
-        </td>
-        <td>
-          <select class="form-select form-select-sm" aria-label="Small select example">
+       <th colspan="3">
+        <select class="form-select form-select-sm" aria-label="Small select example">
             <option value="1">Galindas</option>
             <option value="2">Domicilio</option>
             <option value="2">Instalacion</option>
           </select>
-        </td>
+       </th>
+       <th colspan="2">
+        <select class="form-select form-select-sm" aria-label="Small select example">
+            <option value="1">Galindas</option>
+            <option value="2">Domicilio</option>
+            <option value="2">Instalacion</option>
+          </select>
+       </th>
       </tr>
       <tr class="table-success">
-        <td>PubliG</td>
-        <td>Concepto</td>
-        <td>Subtotal</td>
-        <td id="subTotal">$0.00</td>
+         <td id="txt_cantidad">
+                   <!-- Button trigger modal -->
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            <i class="bi bi-plus-lg"></i>
+        </button>
+            Cantidad
+        </td>
+        <td id="txt_descripcion">Descripcion</td>
+        <td id="txtPreciounitario">Precio Unitario</td>
+        <td class="h5">Total</td>
         <td></td>
       </tr>
+      <tr >
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">PUBLI G</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Cantidad</label>
+                <input type="text" class="form-control" id="txtCantidad" placeholder="cantidad">
+              </div>
+              <div class="mb-3">
+                <label for="exampleFormControlTextarea1" class="form-label">Descripcion</label>
+                <textarea class="form-control" id="txtdescripcion" rows="3"></textarea>
+              </div>
+
+              <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Costo</label>
+                <input type="text" class="form-control" id="txtCosto" placeholder="costo">
+              </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+          <button type="button" class="btn btn-primary"  onclick="Operacion()">Guardar</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div >
+    <tr >
+        <td id="listaProductos"></td>
+        <td>2</td>
+        </div>
+      </tr>
+
+      <tr class="table-success">
+        <td  class="table-success"></td>
+        <td class="table-success"></td>
+        <td class="h5">Subtotal</td>
+        <td id="subTotal" class="h5">$0.00</td>
+        <td class="table-success"></td>
+      </tr>
       <tr>
-        <td></td>
-        <td>
-          <select class="form-select form-select-sm" aria-label="Small select example">
-            <option value="1">ANUNICO 2D</option>
-            <option value="2">ANUNCIO LUMINOSO</option>
-            <option value="2">BANDERA PUBLICITARIA</option>
-            <option value="3">CREDENCIAL</option>
-            <option value="4">DISEÑO</option>
-            <option value="5">ELECTROPLASTICO</option>
-            <option value="6">ESTRUCTURA CON LONA</option>
-            <option value="7">ETIQUETAS</option>
-            <option value="8">IMÁN</option>
-            <option value="9">LETRAS 3D</option>
-            <option value="10">LONA</option>
-            <option value="11">MICROPERFORADO</option>
-            <option value="12">NOTA</option>
-            <option value="13">PLAYERAS</option>
-            <option value="14">PROMOSIONALES</option>
-            <option value="15">SELLO</option>
-            <option value="16">SEÑALATICA</option>
-            <option value="17">SERIGRAFIA</option>
-            <option value="18">TABLOIDES</option>
-            <option value="19">TARJETA</option>
-            <option value="20">TAZAS</option>
-            <option value="21">TEXTIL</option>
-            <option value="22">TOLDO ENROLLABLE</option>
-            <option value="23">TOLDO FIJO</option>
-            <option value="24">VINIL CON SUAJE</option>
-            <option value="25">VINIL CORTE</option>
-          </select>
+        <td class="table-success">
         </td>
-       
-        <td>IVA</td>
-        <td id="txtIva">
+        <td class="table-success">
+        </td>
+        <td class="h5">IVA</td>
+        <td id="txtIva" class="h5">
          $0.00
         </td>
       </tr>
       <tr>
-        <td></td>
-        <td></td>
-        <td>Total</td>
-        <td id="txt_Total">$0.00</td>
+        <td class="table-success"></td>
+        <td class="table-success"></td>
+        <td class="h5">Total</td>
+        <td id="txt_Total" class="h5">$0.00</td>
       </tr>
       <tr>
-        <td></td>
-        <td></td>
-        <td>Anticipo</td>
-        <td id="txt_anticipo">$0.00</td>
+        <td class="table-success"></td>
+        <td class="table-success"></td>
+        <td class="h5">Anticipo</td>
+       <td>
+        <input type="text" class="form-control" placeholder="anticipo">
+    </td>
       </tr>
       <tr>
-        <td></td>
-        <td></td>
-        <td>Por Pagar</td>
-        <td id="txtPorpagar">$0.00</td>
+        <td class="table-success"></td>
+        <td class="table-success"></td>
+        <td class="h5">Por Pagar</td>
+        <td id="txtPorpagar" class="h5">$0.00</td>
       </tr>
     </tbody>
   </table>
+  <hr>
+  <div class="d-flex justify-content-end">
+      <input type="submit" class="btn btn-success" value="Crear Nota">
+  </div>
+  </form>
 </div>
+
 @endsection
